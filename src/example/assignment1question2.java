959b584ad57e4ba25462a6d396db6265d848ece6
Damon Baker <damonbaker@Damons-MacBook-Pro.local>
2019-02-06 01:09:05 -0500

    Initial commit

---
+++ src/example/assignment1question2.java
@@ -0,0 +1,17 @@
+package example;
+
+public class assignment1question2 {
+    public static void main(String[] args) {
+        //2.	Write Java statements that accomplish each of the following tasks:
+             //a.	Display the message “Enter an Integer:”
+             System.out.println("Enter an Integer");
+
+             //b.	Assign the product of variables b and c to variable a.
+             System.out.println("The product of b and c is a, a = b * c");
+
+             //c.	Use a comment to state that a program performs a sample payroll calculation.
+             //This program performs a sample payroll calculation.
+
+    }
+
+}
